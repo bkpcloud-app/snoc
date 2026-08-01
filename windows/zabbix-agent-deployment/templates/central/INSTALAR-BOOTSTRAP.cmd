31004cc23b56b72b2c642803e38b1f2fd309ca94
bkpcloud-app <diegodmanso@gmail.com>
2026-08-01 04:06:43 -0300

    feat(snoc-windows): add bootstrap installation command

---
+++ windows/zabbix-agent-deployment/templates/central/INSTALAR-BOOTSTRAP.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal EnableExtensions
+set "INSTALLER=%~dp0BOOTSTRAP-INSTALL\bootstrap\Install-DDM-SNOC-Bootstrap.ps1"
+if not exist "%INSTALLER%" exit /b 3
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%INSTALLER%" -CentralRoot "%~dp0" -RunNow
+exit /b %ERRORLEVEL%
