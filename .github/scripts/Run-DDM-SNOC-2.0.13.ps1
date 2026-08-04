be4026f07db4dcfdf31c30dfba15b4ad887199f9
bkpcloud-app <diegodmanso@gmail.com>
2026-08-04 19:02:05 -0300

    ci(snoc-windows): corrige marcadores literais da promocao 2.0.13

---
+++ .github/scripts/Run-DDM-SNOC-2.0.13.ps1
@@ -0,0 +1,33 @@
+#requires -Version 5.1
+$ErrorActionPreference='Stop'
+$Path=Join-Path $env:GITHUB_WORKSPACE '.github\scripts\Promote-DDM-SNOC-2.0.13.ps1'
+$Text=[IO.File]::ReadAllText($Path)
+$Utf8=New-Object Text.UTF8Encoding($false)
+
+$Text=[regex]::Replace(
+    $Text,
+    '(?m)^\s*\$Marker="\$Boot=\$DDMProduct\.BootstrapDirectory"\s*$',
+    "        `$Marker='`$Boot=`$DDMProduct.BootstrapDirectory'"
+)
+
+$OldReleaseLine=@'
+    $FirstTest=$FirstTest.Replace("$ReleaseId='2.0.12__7.0.29__FIRSTINSTALL'","$ReleaseId='2.0.13__7.0.29__FIRSTINSTALL'")
+'@
+$NewReleaseLine=@'
+    $FirstTest=$FirstTest.Replace('$ReleaseId=''2.0.12__7.0.29__FIRSTINSTALL''','$ReleaseId=''2.0.13__7.0.29__FIRSTINSTALL''')
+'@
+$Text=$Text.Replace($OldReleaseLine.Trim(),$NewReleaseLine.Trim())
+
+$Text=[regex]::Replace(
+    $Text,
+    '(?m)^\s*\$Needle="\$BootstrapInstaller = Read-DDMRaw ''bootstrap\\Install-DDM-SNOC-Bootstrap\.ps1''"\s*$',
+    "        `$Needle='`$BootstrapInstaller = Read-DDMRaw ''bootstrap\Install-DDM-SNOC-Bootstrap.ps1'''"
+)
+
+if($Text -match '\$Marker="\$Boot='){throw 'Marcador Boot ainda usa interpolacao.'}
+if($Text -match '\$FirstTest\.Replace\("\$ReleaseId='){throw 'ReleaseId do teste ainda usa interpolacao.'}
+if($Text -match '\$Needle="\$BootstrapInstaller'){throw 'Needle BootstrapInstaller ainda usa interpolacao.'}
+
+[IO.File]::WriteAllText($Path,$Text,$Utf8)
+& $Path
+exit $LASTEXITCODE
